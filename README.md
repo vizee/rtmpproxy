268a82dcd1c0a6751832928394532a709f5e2778
vizee <vizee@live.cn>
2018-10-18 18:03:48 +0800

    biu

---
+++ README.md
@@ -0,0 +1,32 @@
+# rtmpproxy
+
+rtmpproxy 通过修改 RTMP 命令(例如 connect 和 publish)中的一些信息, 实现转发 RTMP 流到其他流服务器.
+
+## 结合 dnsproxy 实现 PS4 twitch 流转发
+
+主要思路就是利用 DNS 欺骗 PS4 把发往 twtich 的流转发到 rtmpproxy 上, 再由 rtmpproxy 转发到目标流 URL.
+
+### 1. 首先你要先配置好 PS4 上的 twitch 直播
+
+略.
+
+### 2. PC 运行 dnsproxy
+
+主要是配置 dnsproxy 要劫持的域名到 rtmpproxy 监听的 IP
+
+### 3. 自定配置 PS4 网络
+
+只需要修改 DNS 到 dnsproxy 监听的 IP, 其他配置不动.
+
+### 4. 运行 rtmpproxy
+
+启动参数 `-p` 传入直播完整推流URL+名称(例如: *rtmp://host/app?streamname=live*)
+
+### 注意事项
+
+1. 如果不知道要劫持什么域名, dnsproxy 开 `-V` 日志输出一下就知道了 :)
+2. dnsproxy 要同时监听 udp 和 tcp
+3. 对于 Windows, dnsproxy 编译时候可以直接删掉 USR 信号
+4. 对于 Windows, 手工创建一个 resolve.conf
+5. rtmpproxy 在 Linux 下可以获得 splice buff 加成
+6. 由于 rtmpproxy 在处理 `publish` 后直接转 io.Copy, 所以 `publish` 后的命令无法替换 streamname
---
+++ rtmp.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+)
+
+type rtmpChunkHeader struct {
+	format    uint32
+	csid      uint32
+	timestamp uint32
+	length    uint32
+	typeid    uint32
+	streamid  uint32
+}
+
+func (h *rtmpChunkHeader) asBytes() []byte {
+	n := 1
+	csid := h.csid
+	if csid >= 256+64 {
+		csid = 1
+		n += 2
+	} else if csid >= 64 {
+		csid = 0
+		n += 1
+	}
+	switch h.format {
+	case 0:
+		n += 11
+	case 1:
+		n += 7
+	case 2:
+		n += 3
+	}
+	if h.timestamp >= 0xffffff {
+		n += 4
+	}
+	data := make([]byte, n)
+	data[0] = byte((h.format << 6) | csid)
+	p := 1
+	if csid <= 1 {
+		binary.BigEndian.PutUint16(data[1:], uint16(h.csid-64))
+		p += int(csid) + 1
+	}
+	ts := h.timestamp
+	exts := uint32(0)
+	if ts >= 0xffffff {
+		ts = 0xffffff
+		exts = ts
+	}
+	switch h.format {
+	case 0, 1:
+		binary.BigEndian.PutUint32(data[p:], ts<<8)
+		binary.BigEndian.PutUint32(data[p+3:], h.length<<8)
+		data[p+6] = byte(h.typeid)
+		if h.format == 0 {
+			binary.LittleEndian.PutUint32(data[p+7:], h.streamid)
+			p += 11
+		} else {
+			p += 7
+		}
+	case 2:
+		binary.BigEndian.PutUint16(data[p:], uint16(ts>>8))
+		data[p+2] = byte(h.timestamp & 0xff)
+		p += 3
+	}
+	if ts == 0xffffff && h.format != 3 {
+		binary.BigEndian.PutUint32(data[p:], exts)
+	}
+	return data
+}
+
+func rtmpReadHeader(r io.Reader) (*rtmpChunkHeader, error) {
+	var buf [18]byte
+	_, err := io.ReadFull(r, buf[:1])
+	if err != nil {
+		return nil, err
+	}
+	format := uint32(buf[0] >> 6)
+	csid := uint32(buf[0] & 0x3f)
+	n := 0
+	if csid <= 1 {
+		n += int(csid) + 1
+	}
+	switch format {
+	case 0:
+		n += 11
+	case 1:
+		n += 7
+	case 2:
+		n += 3
+	}
+	ch := &rtmpChunkHeader{
+		format: format,
+		csid:   csid,
+	}
+	if n > 0 {
+		_, err := io.ReadFull(r, buf[:n])
+		if err != nil {
+			return nil, err
+		}
+		p := 0
+		switch csid {
+		case 0:
+			ch.csid = uint32(buf[0]) + 64
+			p = 1
+		case 1:
+			ch.csid = uint32(binary.BigEndian.Uint16(buf[:])) + 64
+			p = 2
+		}
+		hbuf := buf[p:]
+		switch format {
+		case 0:
+			_ = hbuf[:11]
+			ch.timestamp = binary.BigEndian.Uint32(hbuf) >> 8
+			ch.length = binary.BigEndian.Uint32(hbuf[3:]) >> 8
+			ch.typeid = uint32(hbuf[6])
+			ch.streamid = binary.LittleEndian.Uint32(hbuf[7:])
+		case 1:
+			_ = hbuf[:7]
+			ch.timestamp = binary.BigEndian.Uint32(hbuf) >> 8
+			ch.length = binary.BigEndian.Uint32(hbuf[3:]) >> 8
+			ch.typeid = uint32(hbuf[6])
+		case 2:
+			_ = hbuf[:3]
+			ch.timestamp = binary.BigEndian.Uint32(hbuf) >> 8
+		}
+		if ch.timestamp == 0xffffff {
+			_, err := io.ReadFull(r, buf[:4])
+			if err != nil {
+				return nil, err
+			}
+			ch.timestamp = binary.BigEndian.Uint32(buf[:4])
+		}
+	}
+	return ch, nil
+}
+
+func writeRtmpMessage(w io.Writer, ch *rtmpChunkHeader, payload []byte, chunkSize int) error {
+	nwrote := 0
+	for nwrote < len(payload) {
+		if nwrote == 0 {
+			ch.format = 0
+			ch.length = uint32(len(payload))
+		} else {
+			ch.format = 3
+		}
+		_, err := w.Write(ch.asBytes())
+		if err != nil {
+			return err
+		}
+		n := chunkSize
+		if len(payload)-nwrote < chunkSize {
+			n = len(payload) - nwrote
+		}
+		_, err = w.Write(payload[nwrote : nwrote+n])
+		if err != nil {
+			return err
+		}
+		nwrote += n
+	}
+	return nil
+}
+
+func shadowHandshake(conn net.Conn, conn2 net.Conn) error {
+	errs := make(chan error, 2)
+	copyfn := func(conn net.Conn, conn2 net.Conn) {
+		_, err := io.CopyN(conn, conn2, 1+1536+1536)
+		errs <- err
+	}
+	go copyfn(conn, conn2)
+	go copyfn(conn2, conn)
+	e1 := <-errs
+	if e1 != nil {
+		return e1
+	}
+	e2 := <-errs
+	if e2 != nil {
+		return e2
+	}
+	return nil
+}
